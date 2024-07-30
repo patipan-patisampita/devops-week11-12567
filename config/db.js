5be1a927a727557926bd9d7bc7d95644697aab29
patipan <patipan.patisampita@gmail.com>
2024-07-30 12:02:05 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config()
 
 // Create the connection pool. The pool-specific settings are the defaults
 const connect = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database: 'db-express',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 // For pool initialization, see above
